fdeda18f7ce3860573bb0ee46a615d52c59abce4
steveeum <steveeum@gmail.com>
2021-04-28 02:13:49 -0400

    first component/page done

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DrawCard from "./Components/DrawCard";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <DrawCard />
+      </div>
+    </>
+  );
+}
+
+export default App;
